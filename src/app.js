fcb8d1a9911bef696e3a29b535f5d012824f2241
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-06-04 09:26:44 +0000

    Adding excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "My colleague",
+    "My boss",
+    "My supervisor",
+    "A bird",
+    "The English teacher"
+  ];
+  let action = ["borrowed", "took", "edited", "changed", "denied"];
+  let what = [
+    "my meeting notes",
+    "my laptop",
+    "my coffee",
+    "my lunch",
+    "the snacks"
+  ];
+  let when = [
+    "during the meeting",
+    "during the stand-up",
+    "while I was eating my lunch",
+    "during my break",
+    "outside of my working hours"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById("excuse").innerHTML =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
 };
